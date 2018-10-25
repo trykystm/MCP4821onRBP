dbed8aae45c7cd9a4a8eb5454a6c16464a57775d
trykystm <trykystm@gmail.com>
2018-10-25 22:04:38 +0900

    Update mcp4821_RBP.rb

--- mcp4821_RBP.rb
+++ mcp4821_RBP.rb
@@ -19,27 +19,6 @@ class MCP4821 < PiPiper::Spi
     second = command & 0b0000000011111111
     @spi.write first, second
   end
-  
-  def latch
-  end
-  
-  def shdn
-    @spi.write 0b0001000, 0b000000000
-  end
-  
-  def hw_shdn
-  end
-  
-  def hw_shdn_release
-  end
-  
-  def set_hw_shdn_pin(pin)
-    @hw_shdn_pin = pin
-  end
-  
-  def set_latch_pin(pin)
-    @latch_pin = pin
-  end
 
   if __FILE__ == $0
     mcp4821 = self.new CE1
