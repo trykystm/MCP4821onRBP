57aecd6d299c3212ee2c3affef8912307208e835
trykystm <trykystm@gmail.com>
2018-10-25 09:11:41 +0900

    Create mcp4821_RBP.rb
    first

---
+++ mcp4821_RBP.rb
@@ -0,0 +1,36 @@
+require 'pi_piper'
+#include 'PiPiper::Spi'
+
+class MCP4821 < PiPiper::Spi
+  
+  CE0 = CHIP_SELECT_0
+  CE1 = CHIP_SELECT_1
+  
+  def initialize(chip = CE0)
+    PiPiper::Spi.begin(chip) do |spi|
+      @spi = spi
+    end
+  end
+  
+  def write(dac)
+    command = 0b0011000000000000 + dac
+    first = command >> 8
+    second = command & 0b0000000011111111
+    @spi.write first, second
+  end
+  
+  def shdn
+    @spi.write 0b0001000, 0b000000000
+  end
+
+  if __FILE__ == $0
+    mcp4821 = self.new CE1
+    (0..4095).each do |dac|
+      mcp4821.write dac
+    end
+    sleep 5
+    mcp4821.shdn
+  end
+  
+end
+
