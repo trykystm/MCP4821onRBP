b465f9d42cae1f6e37c9cb66cf099812a2e4b967
trykystm <trykystm@gmail.com>
2018-11-28 20:58:48 +0900

    Update mcp4821_RBP_spec.rb

--- mcp4821_RBP_spec.rb
+++ mcp4821_RBP_spec.rb
@@ -1,7 +1,10 @@
-describe MCP4821 do
+require 'mcp4821_RBP'
+RSpec.describe MCP4821 do
   describe '#write(dac)' do
-    subject {mcp4821.write(dac)}
-    let(:mcp4821){MCP4821.new}
+    subject do
+      mcp4821 = MCP4821.new
+      mcp4821.write(dac)
+    end
     context 'when dac is 1845' do
       let(:dac){1845}
       it {is_expected.to eq }
